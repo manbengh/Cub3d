91b718a565829361f7ef325eceaacf81ed325fe6
Manar Bengharbi <manbengh@bess-f2r1s21.clusters.42paris.fr>
2025-03-19 17:25:56 +0100

    rotate functions r & l

--- srcs/my_game.c
+++ srcs/my_game.c
@@ -177,6 +177,42 @@ void    move_left(t_cub *cub, t_mlx *my_mlx)
         my_mlx->pos.y -= my_mlx->dir.x * PLAYER_SPEED;
 }
 
+void    to_the_right(t_mlx *my_mlx)
+{
+    double  old_dir_x;
+    double  old_plane_x;
+
+    old_dir_x = my_mlx->dir.x;
+    old_plane_x = my_mlx->plane.x;
+    my_mlx->dir.x = my_mlx->dir.x * cos(ROTATION_SPEED) - my_mlx->dir.y * sin(ROTATION_SPEED);
+    my_mlx->dir.y = old_dir_x * sin(ROTATION_SPEED) + my_mlx->dir.y * cos(ROTATION_SPEED);
+    my_mlx->plane.x = my_mlx->plane.x * cos(ROTATION_SPEED) - my_mlx->plane.y * sin(ROTATION_SPEED);
+    my_mlx->plane.y = old_plane_x * sin(ROTATION_SPEED) + my_mlx->plane.y * cos(ROTATION_SPEED);
+}
+
+void    to_the_left(t_mlx *my_mlx)
+{
+    double  old_dir_x;
+    double  old_plane_x;
+
+    old_dir_x = my_mlx->dir.x;
+    old_plane_x = my_mlx->plane.x;
+    my_mlx->dir.x = my_mlx->dir.x * cos(-ROTATION_SPEED) - my_mlx->dir.y * sin(-ROTATION_SPEED);
+    my_mlx->dir.y = old_dir_x * sin(-ROTATION_SPEED) + my_mlx->dir.y * cos(-ROTATION_SPEED);
+    my_mlx->plane.x = my_mlx->plane.x * cos(-ROTATION_SPEED) - my_mlx->plane.y * sin(-ROTATION_SPEED);
+    my_mlx->plane.y = old_plane_x * sin(-ROTATION_SPEED) + my_mlx->plane.y * cos(-ROTATION_SPEED);
+}
+
+void    rotation(t_cub *cub)
+{
+    if (cub->maps->my_map[(int)(cub->my_mlx->pos.y)][(int)(cub->my_mlx->pos.x)] == 1)
+        return ;
+    if (cub->keys->rotate == 1)
+        to_the_left(cub);
+    else if (cub->keys->rotate == -1)
+        to_the_right(cub);
+}
+
 int moving(t_cub *cub)
 {
     if (cub->keys->w == 1)
@@ -190,12 +226,12 @@ int moving(t_cub *cub)
     if (cub->keys->left == 1)
     {
         cub->keys->rotate = -1;
-        //rotate_function
+        rotation(cub);
     }
     if (cub->keys->right == 1)
     {
         cub->keys->rotate = 1;
-        //rotate_function
+        rotation(cub);
     }
     return(0);
 }
